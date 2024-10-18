a4764d826c2ce4a0c19b1847e716b33901a86c90
Andrew Nutt <87488155+anutt12@users.noreply.github.com>
2024-10-18 11:04:11 -0700

    Initial code

---
+++ src/main/java/org/twitterbot/knifeyspoony/KnifeySpoonyApplication.java
@@ -0,0 +1,13 @@
+package org.twitterbot.knifeyspoony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnifeySpoonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnifeySpoonyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/twitterbot/knifeyspoony/KnifeySpoonyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.twitterbot.knifeyspoony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnifeySpoonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
